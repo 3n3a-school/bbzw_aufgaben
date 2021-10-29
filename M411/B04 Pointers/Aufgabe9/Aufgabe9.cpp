608ae50f130cb85389885beb683fddd16ce94543
3n3a <enea_kraehenbuehl@sluz.ch>
2021-10-29 08:55:49 +0000

    moved fgets into loop

--- M411/B04 Pointers/Aufgabe9/Aufgabe9.cpp
+++ M411/B04 Pointers/Aufgabe9/Aufgabe9.cpp
@@ -6,10 +6,10 @@ int main() {
   char text_input[50];
 
   printf("Enter string to be uppercase: ");
-  fgets(text_input, sizeof(text_input), stdin);
+  //fgets(text_input, sizeof(text_input), stdin);
   //gets_s(text_input);
 
-  for (char* pt = text_input; *pt != '\0' && *pt != 0x0a;) (*pt >= 'a' && *pt <= 'z') ? (*pt++ -= 0x20) : *pt++;
+  for (char* pt = fgets(text_input, sizeof(text_input), stdin); *pt != '\0' && *pt != 0x0a;) (*pt >= 'a' && *pt <= 'z') ? (*pt++ -= 0x20) : *pt++;
   
   printf("Uppercase String: %s", text_input);
 
