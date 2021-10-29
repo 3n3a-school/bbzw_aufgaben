cce16b18fe756abbcb077a5d7238202792554a9d
enk <enea.kraehenbuehl@suva.ch>
2021-10-29 10:14:16 +0200

    added new option in a8

--- M411/B04 Pointers/Aufgabe8/Aufgabe8.cpp
+++ M411/B04 Pointers/Aufgabe8/Aufgabe8.cpp
@@ -2,13 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+void removePrependedSpaces() {
   char text_input[50], text_output[50];
 
   printf("Enter string to be cleaned: ");
   fgets(text_input, sizeof(text_input), stdin);
   //gets_s(text_input);
+  char *pSrc = Text;
+  char *pDst = Text;
 
+  while (*pSrc == ' ') pSrc++;
+  while(*pSrc != '\0') {
+    *pDst = *pSrc;
+    pSrc++;
+    pDst++;
+  }
+  printf("Cleaned String: %s", text_output);
+}
+
+void removeAllSpaces() {
+  char text_input[50], text_output[50];
+
+  printf("Enter string to be cleaned: ");
+  fgets(text_input, sizeof(text_input), stdin);
+  //gets_s(text_input);
   int i = 0;
   int array_pos = 0;
   char* pt = text_input;
@@ -21,9 +38,20 @@ int main() {
     i++;
     pt++;
   }
-  
   printf("Cleaned String: %s", text_output);
+}
 
+int main() {
+  char chosen;
+  printf("Choose option: ");
+  fgets(chosen, sizeof(chosen), stdin);
+  
+  if (chosen == 'a') {
+    removeAllSpaces();
+  } else {
+    removePrependedSpaces();
+  }
+  
   //system("pause");
   return 0;
 }
